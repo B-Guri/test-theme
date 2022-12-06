0a2e8ef060f8328506345b34ae63a8d566998774
Bohdan <hursky73@gmail.com>
2022-12-06 14:47:23 +0200

    fix

--- assets/custom-video.js
+++ assets/custom-video.js
@@ -3,8 +3,6 @@ let playerContainer = document.querySelector(".custom-video__content");
 playerContainer.addEventListener("click", () => {
   let player = playerContainer.querySelector(".video-player");
   let playButton = playerContainer.querySelector(".play-button");
-  // player = document.querySelector(".video-player");
-  console.log("click");
   let command;
   if (playButton.classList.contains("on-pause")) {
     if (player.classList.contains("yt")) {
