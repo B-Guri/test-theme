278fbf4906e223bebc84b22b1bc36e280f7edd8c
Bohdan <hursky73@gmail.com>
2022-12-06 14:21:36 +0200

    fix

--- assets/custom-video.js
+++ assets/custom-video.js
@@ -1,29 +1,53 @@
-let playerContainers = document.querySelectorAll(".custom-video__content");
+// let playerContainers = document.querySelectorAll(".custom-video__content");
 
-for (let i = 0; i < playerContainers.length; i++) {
-  playerContainers[i].addEventListener("click", () => {
-    let player = playerContainers[i].querySelector(".video-player");
-    let playButton = playerContainers[i].querySelector(".play-button");
-    // player = document.querySelector(".video-player");
-    console.log("click");
-    let command;
-    if (playButton.classList.contains("on-pause")) {
-      if (player.classList.contains("yt")) {
-        command = playVideo("youtube");
-      } else if (player.classList.contains("vm")) {
-        command = playVideo("vimeo");
-      }
-    } else {
-      if (player.classList.contains("yt")) {
-        command = pauseVideo("youtube");
-      } else if (player.classList.contains("vm")) {
-        command = pauseVideo("vimeo");
-      }
+// for (let i = 0; i < playerContainers.length; i++) {
+//   playerContainers[i].addEventListener("click", () => {
+//     let player = playerContainers[i].querySelector(".video-player");
+//     let playButton = playerContainers[i].querySelector(".play-button");
+//     // player = document.querySelector(".video-player");
+//     console.log("click");
+//     let command;
+//     if (playButton.classList.contains("on-pause")) {
+//       if (player.classList.contains("yt")) {
+//         command = playVideo("youtube");
+//       } else if (player.classList.contains("vm")) {
+//         command = playVideo("vimeo");
+//       }
+//     } else {
+//       if (player.classList.contains("yt")) {
+//         command = pauseVideo("youtube");
+//       } else if (player.classList.contains("vm")) {
+//         command = pauseVideo("vimeo");
+//       }
+//     }
+//     playButton.classList.toggle("on-pause");
+//     player.contentWindow.postMessage(JSON.stringify(command), "*");
+//   });
+// }
+let playerContainer = document.querySelector(".custom-video__content");
+
+playerContainer.addEventListener("click", () => {
+  let player = playerContainer.querySelector(".video-player");
+  let playButton = playerContainer.querySelector(".play-button");
+  // player = document.querySelector(".video-player");
+  console.log("click");
+  let command;
+  if (playButton.classList.contains("on-pause")) {
+    if (player.classList.contains("yt")) {
+      command = playVideo("youtube");
+    } else if (player.classList.contains("vm")) {
+      command = playVideo("vimeo");
     }
-    playButton.classList.toggle("on-pause");
-    player.contentWindow.postMessage(JSON.stringify(command), "*");
-  });
-}
+  } else {
+    if (player.classList.contains("yt")) {
+      command = pauseVideo("youtube");
+    } else if (player.classList.contains("vm")) {
+      command = pauseVideo("vimeo");
+    }
+  }
+  playButton.classList.toggle("on-pause");
+  player.contentWindow.postMessage(JSON.stringify(command), "*");
+});
 
 function pauseVideo(platform) {
   let command;
